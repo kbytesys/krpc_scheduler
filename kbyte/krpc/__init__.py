2a180ca04ab1e6aee588dab82f120823d28ed4c7
Andrea Briganti <kbytesys@gmail.com>
2015-05-12 20:16:51 +0200

    better doc

--- kbyte/krpc/__init__.py
+++ kbyte/krpc/__init__.py
@@ -4,11 +4,14 @@ from threading import Thread, RLock
 import time
 import logging
 
-"""
-Base vessel telemetry class with some common streamed data. You can still subclass or add attributes if you need more
-telemetry data
-"""
+log = logging.getLogger('krpc_scheduler')
+
+
 class KRPCVesselTelemetry():
+    """
+    Base vessel telemetry class with some common streamed data. You can still subclass or add attributes if you need more
+    telemetry data
+    """
     def __init__(self, conn, vessel):
         # Base telemetry
         self.conn = conn
@@ -22,20 +25,24 @@ class KRPCVesselTelemetry():
         self.eccentricity = conn.add_stream(getattr, vessel.orbit, 'eccentricity')
         self.vessel_orbit_speed = conn.add_stream(getattr, vessel.flight(vessel.orbit.body.reference_frame), 'speed')
 
-    """
-    Add a new attribute from vessel.flight() generic data
-    """
+
     def add_vessel_flight_telemetry(self, attrib_name, proto_name):
+        """
+        Add a new attribute from vessel.flight() generic data
+        """
         setattr(self, attrib_name, self.conn.add_stream(getattr, self.vessel.flight(), proto_name))
 
+class KRPCVesselScheduler(Thread):
+    pass
 
-"""
-Abstract class for scheduler jobs. You can create stage related jobs or always active jobs. Every job have got also an
-id (you have to assure that it is unique, if you want to remove them by id) and a priority value. Greater priority value
-means higher priority. You can disable/enable the job at your choice. When you set the expired field to true, the
-scheduler will remove the job.
-"""
+
 class KRPCJob():
+    """
+    Abstract class for scheduler jobs. You can create stage related jobs or always active jobs. Every job have got also an
+    id (you have to assure that it is unique, if you want to remove them by id) and a priority value. Greater priority value
+    means higher priority. You can disable/enable the job at your choice. When you set the expired field to true, the
+    scheduler will remove the job.
+    """
     def __init__(self, jobid, priority=10, stage=None):
         self.priority = priority
         self.stage = stage
@@ -43,45 +50,45 @@ class KRPCJob():
         self.disabled = False
         self.expired = False
 
-    """
-    Execute the job. You can perform every action to vessel through the krpc api, but don't change the stage here. Use
-    the cycle_commands set instead. If you add "nextstage" to the set, the scheduler will perform a stage change at
-    end of its cycle. You can also add "shutdown" to abort the whole scheduler or you can add custom string command
-    that will be managed by jobs at the end of scheduler cycle.
-    """
     def execute(self, telemetry: KRPCVesselTelemetry, scheduler: KRPCVesselScheduler, stage, cycle_commands: set):
+        """
+        Execute the job. You can perform every action to vessel through the krpc api, but don't change the stage here. Use
+        the cycle_commands set instead. If you add "nextstage" to the set, the scheduler will perform a stage change at
+        end of its cycle. You can also add "shutdown" to abort the whole scheduler or you can add custom string command
+        that will be managed by jobs at the end of scheduler cycle.
+        """
         pass
 
-    """
-    Perform commands at stage change. Look execute method doc for more info.
-    """
     def execute_exit_stage(self, telemetry: KRPCVesselTelemetry, scheduler: KRPCVesselScheduler, stage, cycle_commands: set):
+        """
+        Perform commands at stage change. Look execute method doc for more info.
+        """
         pass
 
-    """
-    Perform commands at stage change. Look execute method doc for more info.
-    """
     def execute_enter_stage(self, telemetry: KRPCVesselTelemetry, scheduler: KRPCVesselScheduler, stage, cycle_commands: set):
+        """
+        Perform commands at stage change. Look execute method doc for more info.
+        """
         pass
 
-    """
-    Execute a custom command at end of scheduler cycle. Look execute method doc for more info.
-    """
     def execute_custom_command(self, telemetry: KRPCVesselTelemetry, scheduler: KRPCVesselScheduler, stage, command):
+        """
+        Execute a custom command at end of scheduler cycle. Look execute method doc for more info.
+        """
         pass
 
-    """
-    Method called when the scheduler remove this job. You can use it when you need to perform some cleanups.
-    """
     def remove_from_scheduler(self, scheduler: KRPCVesselScheduler):
+        """
+        Method called when the scheduler remove this job. You can use it when you need to perform some cleanups.
+        """
         pass
 
 
 # noinspection PyUnresolvedReferences
-"""
-Scheduler class, you can perform scheduler jobs with a configurable frequency
-"""
 class KRPCVesselScheduler(Thread):
+    """
+    Scheduler class, you can perform scheduler jobs with a configurable frequency
+    """
     def __init__(self, conn: krpc.client.Client, telemetry: KRPCVesselTelemetry=None, frequency=0.5):
         Thread.__init__(self)
         self.conn = conn
@@ -96,19 +103,19 @@ class KRPCVesselScheduler(Thread):
         if telemetry is None:
             self.telemetry = KRPCVesselTelemetry(self.conn, self.vessel)
 
-    """
-    Register a job in the scheduler
-    """
     def register_job(self, job: KRPCJob):
+        """
+        Register a job in the scheduler
+        """
         self.queuelock.acquire()
         self.joblist.append(job)
         self.joblist.sort(key=lambda x: x.priority, reverse=True)
         self.queuelock.release()
 
-    """
-    Remove a job from the scheduler by jobid
-    """
     def unregister_job(self, jobid):
+        """
+        Remove a job from the scheduler by jobid
+        """
         self.queuelock.acquire()
         for job in self.joblist:
             if job.jobid == jobid:
@@ -117,10 +124,10 @@ class KRPCVesselScheduler(Thread):
                 break
         self.queuelock.release()
 
-    """
-    Remove a job from the scheduler by job object
-    """
     def unregister_job_object(self, job: KRPCJob):
+        """
+        Remove a job from the scheduler by job object
+        """
         self.queuelock.acquire()
         try:
             self.joblist.remove(job)
@@ -129,12 +136,12 @@ class KRPCVesselScheduler(Thread):
             pass
         self.queuelock.release()
 
-    """
-    Run the scheduler. In every cycle the scheduler manege the job list cleanup from expired items, manage stage changes,
-    execute allowed jobs by status and current stage and perform end cycle commands requested by jobs. Please check the
-    KRPCJob doc for better infos.
-    """
     def run(self):
+        """
+        Run the scheduler. In every cycle the scheduler manege the job list cleanup from expired items, manage stage changes,
+        execute allowed jobs by status and current stage and perform end cycle commands requested by jobs. Please check the
+        KRPCJob doc for better infos.
+        """
         self.shutdown = False
         scheduler_stage = self.telemetry.stage()
         while not self.shutdown:
@@ -151,11 +158,11 @@ class KRPCVesselScheduler(Thread):
 
                 if not job.disabled and not job.expired and (job.stage is None or job.stage == scheduler_stage):
                     cloned_jobs.append(job)
-                    logging.debug("Job %s allowed in this scheduler cycle" % job.jobid)
+                    log.debug("Job %s allowed in this scheduler cycle" % job.jobid)
 
             for job in toremove:
                 self.unregister_job_object(job)
-                logging.debug("Job %s removed from scheduler." % job.jobid)
+                log.debug("Job %s removed from scheduler." % job.jobid)
 
             self.queuelock.release()
 
@@ -164,7 +171,7 @@ class KRPCVesselScheduler(Thread):
             reverted the flight. Abort the scheduler to avoid an atomic fallout.
             '''
             if scheduler_stage < self.telemetry.stage():
-                logging.error("Scheduler stage not coerent!!! Vechile switch???")
+                log.error("Scheduler stage not coerent!!! Vechile switch???")
                 cycle_commands = ('shutdown',)
 
             else:
@@ -176,19 +183,19 @@ class KRPCVesselScheduler(Thread):
                 '''
                 if scheduler_stage != self.telemetry.stage():
                     while scheduler_stage != self.telemetry.stage():
-                        logging.info("Change stage from %d to %d" % (scheduler_stage, self.telemetry.stage()))
+                        log.info("Change stage from %d to %d" % (scheduler_stage, self.telemetry.stage()))
                         for job in cloned_jobs:
                             try:
                                 job.execute_exit_stage(self.telemetry, self, scheduler_stage, cycle_commands)
                             except Exception as e:
-                                logging.error("Job Exit Stage error: jobid %s stage %s error %s" %
+                                log.error("Job Exit Stage error: jobid %s stage %s error %s" %
                                               (job.jobid, scheduler_stage, e))
                         scheduler_stage -= 1
                         for job in cloned_jobs:
                             try:
                                 job.execute_enter_stage(self.telemetry, self, scheduler_stage, cycle_commands)
                             except Exception as e:
-                                logging.error("Job Enter Stage error: jobid %s stage %s error %s" %
+                                log.error("Job Enter Stage error: jobid %s stage %s error %s" %
                                               (job.jobid, scheduler_stage, e))
 
                 '''
@@ -200,14 +207,14 @@ class KRPCVesselScheduler(Thread):
                     try:
                         job.execute(self.telemetry, self, scheduler_stage, cycle_commands)
                     except Exception as e:
-                        logging.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
+                        log.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
 
             '''
             Execute cycle commands added from the jobs. Nextstage and shutdown are handled by the scheduler and
             will not be propagated to jobs.
             '''
             for cmd in cycle_commands:
-                logging.debug('Command %s triggered' % cmd)
+                log.debug('Command %s triggered' % cmd)
                 if cmd == 'nextstage':
                     if scheduler_stage == self.telemetry.stage():
                         self.vessel.control.activate_next_stage()
@@ -219,7 +226,7 @@ class KRPCVesselScheduler(Thread):
                             try:
                                 job.execute_custom_command(self.telemetry, self, scheduler_stage, cmd)
                             except Exception as e:
-                                logging.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
+                                log.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
 
             time.sleep(self.frequency)
 
---
+++ samples/sample_contract.py
@@ -0,0 +1,88 @@
+"""
+Sample contract jobs. We need to test the decouple at 13000 <= altitude <= 14000 with 300 <= speed <= 1200.
+We want to handle prelanch, solid booster and reenter too.
+"""
+
+
+from kbyte.krpc import *
+import krpc
+conn = krpc.connect(name='Contract Experiment')
+import logging
+import sys
+
+# use console for log
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+kslog = logging.getLogger("krpc_scheduler")
+kslog.setLevel(logging.DEBUG)
+log = logging.getLogger("main")
+
+class SolidDetach(KRPCJob):
+    def __init__(self):
+        KRPCJob.__init__(self, 'detach_booster')
+
+    def execute(self, telemetry, scheduler, stage, cycle_commands: set):
+        if telemetry.vessel.resources.amount('SolidFuel') == 0:
+            kslog.info('Booster separation')
+            self.disabled = True
+            self.expired = True
+            cycle_commands.add('nextstage')
+
+
+class PreLaunch(KRPCJob):
+    def __init__(self):
+        KRPCJob.__init__(self, 'prelaunch_settings')
+
+    def execute(self, telemetry, scheduler, stage, cycle_commands):
+        telemetry.vessel.control.sas = True
+        telemetry.vessel.control.throttle = 1
+        self.expired = True
+
+class ExperimentJob(KRPCJob):
+    def __init__(self):
+        KRPCJob.__init__(self, 'test_part', stage=2)
+
+    def execute(self, telemetry, scheduler, stage, cycle_commands: set):
+        kslog.debug("speed: %s\nalt: %s" % (telemetry.vessel_orbit_speed(), telemetry.altitude()))
+        if telemetry.altitude() > 14000:
+            kslog.warning("Experiment failed!!! ABORT ABORT")
+            telemetry.vessel.control.throttle = 0
+            telemetry.vessel.control.sas = False
+            cycle_commands.add('shutdown')
+            return
+
+        if telemetry.altitude() >= 13000:
+            if 300 <= telemetry.vessel_orbit_speed() <= 1200:
+                kslog.info("Engage decoupler.")
+                telemetry.vessel.control.sas = False
+                telemetry.vessel.control.throttle = 0
+                time.sleep(0.1)
+                cycle_commands.add('nextstage')
+                self.disabled = True
+
+
+class DeployPara(KRPCJob):
+    def __init__(self):
+        KRPCJob.__init__(self, 'mk16deploy', stage=1)
+
+    def execute(self, telemetry, scheduler, stage, cycle_commands: set):
+        if telemetry.altitude() <= 5000:
+            kslog.info("Deploy chutes")
+            cycle_commands.add('nextstage')
+            cycle_commands.add('shutdown')
+
+scheduler = KRPCVesselScheduler(conn)
+scheduler.register_job(SolidDetach())
+scheduler.register_job(PreLaunch())
+scheduler.register_job(ExperimentJob())
+scheduler.register_job(DeployPara())
+scheduler.start()
+scheduler.join()
+
+
