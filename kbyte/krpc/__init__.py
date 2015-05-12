7c766ac1e3aafbed1ef137bcb851f401145e1697
Andrea Briganti <kbytesys@gmail.com>
2015-05-12 17:20:54 +0200

    Some scheduler rework

--- kbyte/krpc/__init__.py
+++ kbyte/krpc/__init__.py
@@ -4,7 +4,7 @@ import krpc.client
 import krpc.service
 from threading import Thread, RLock
 import time
-
+import logging
 
 class KRPCVesselTelemetry():
     def __init__(self, conn, vessel):
@@ -36,10 +36,10 @@ class KRPCJob():
     def execute(self, telemetry, scheduler, stage, cycle_commands: set):
         pass
 
-    def execute_exit_stage(self, telemetry, scheduler, stage):
+    def execute_exit_stage(self, telemetry, scheduler, stage, cycle_commands: set):
         pass
 
-    def execute_enter_stage(self, telemetry, scheduler, stage):
+    def execute_enter_stage(self, telemetry, scheduler, stage, cycle_commands: set):
         pass
 
     def execute_custom_command(self, telemetry, scheduler, stage, command):
@@ -47,6 +47,9 @@ class KRPCJob():
 
 
 # noinspection PyUnresolvedReferences
+"""
+Scheduler class, you can perform scheduler jobs with a configurable frequency
+"""
 class KRPCVesselScheduler(Thread):
     def __init__(self, conn: krpc.client.Client, telemetry: KRPCVesselTelemetry=None, frequency=0.5):
         Thread.__init__(self)
@@ -55,79 +58,137 @@ class KRPCVesselScheduler(Thread):
         self.space_center = conn.space_center
         self.vessel = conn.space_center.active_vessel
         self.frequency = frequency
-        self.jobqueue = []
+        self.joblist = list()
         self.queuelock = RLock()
         self.shutdown = True
 
         if telemetry is None:
             self.telemetry = KRPCVesselTelemetry(self.conn, self.vessel)
 
+    """
+    Register a job in the scheduler
+    """
     def register_job(self, job: KRPCJob):
         self.queuelock.acquire()
-        self.jobqueue.append(job)
-        self.jobqueue.sort(key=lambda x: x.priority, reverse=True)
+        self.joblist.append(job)
+        self.joblist.sort(key=lambda x: x.priority, reverse=True)
         self.queuelock.release()
 
+    """
+    Remove a job from the scheduler by jobid
+    """
     def unregister_job(self, jobid):
         self.queuelock.acquire()
-        for job in self.jobqueue:
+        for job in self.joblist:
             if job.jobid == jobid:
-                self.jobqueue.remove(job)
+                self.joblist.remove(job)
                 break
         self.queuelock.release()
 
+    """
+    Remove a job from the scheduler by job object
+    """
+    def unregister_job_object(self, job: KRPCJob):
+        self.queuelock.acquire()
+        try:
+            self.joblist.remove(job)
+        except ValueError:
+            pass
+        self.queuelock.release()
+
+    """
+    Run the scheduler. In every cycle the scheduler manege the job list cleanup from expired items, manage stage changes,
+    execute allowed jobs by status and current stage and perform end cycle commands requested by jobs. Please check the
+    KRPCJob doc for better infos.
+    """
     def run(self):
         self.shutdown = False
         scheduler_stage = self.telemetry.stage()
         while not self.shutdown:
             self.queuelock.acquire()
-            try:
-                if scheduler_stage < self.telemetry.stage():
-                    print("Scheduler stage not coerent!!! Vechile switch???")
-                    self.shutdown = True
-                    self.queuelock.release()
-                    break
+            '''
+            We want to assure list coherence and thread safe ops. First we clone the jobs list and perform some stage
+            related ops. This cycle also purge expired elements.
+            '''
+            cloned_jobs = list()
+            toremove = list()
+            for job in self.joblist:
+                if job.expired:
+                    toremove.append(job)
 
+                if not job.disabled and not job.expired and (job.stage is None or job.stage == scheduler_stage):
+                    cloned_jobs.append(job)
+                    logging.debug("Job %s allowed in this scheduler cycle" % job.jobid)
+
+            for job in toremove:
+                self.unregister_job_object(job)
+                logging.debug("Job %s removed from scheduler." % job.jobid)
+
+            self.queuelock.release()
+
+            '''
+            Check the current stage, if the scheduler stage is minor of telemetry stage, we have switched vessel or
+            reverted the flight. Abort the scheduler to avoid an atomic fallout.
+            '''
+            if scheduler_stage < self.telemetry.stage():
+                logging.error("Scheduler stage not coerent!!! Vechile switch???")
+                cycle_commands = ('shutdown',)
+
+            else:
+
+                cycle_commands = set()
+
+                '''
+                React to stage changes, execute job's exit and enter events until the right stage is reached.
+                '''
                 if scheduler_stage != self.telemetry.stage():
                     while scheduler_stage != self.telemetry.stage():
-                        print("Change stage from %d to %d" % (scheduler_stage, self.telemetry.stage()))
-                        for job in self.jobqueue:
-                            job.execute_exit_stage(self.telemetry, self, scheduler_stage)
+                        logging.info("Change stage from %d to %d" % (scheduler_stage, self.telemetry.stage()))
+                        for job in cloned_jobs:
+                            try:
+                                job.execute_exit_stage(self.telemetry, self, scheduler_stage, cycle_commands)
+                            except Exception as e:
+                                logging.error("Job Exit Stage error: jobid %s stage %s error %s" %
+                                              (job.jobid, scheduler_stage, e))
                         scheduler_stage -= 1
-                        for job in self.jobqueue:
-                            job.execute_enter_stage(self.telemetry, self, scheduler_stage)
+                        for job in cloned_jobs:
+                            try:
+                                job.execute_enter_stage(self.telemetry, self, scheduler_stage, cycle_commands)
+                            except Exception as e:
+                                logging.error("Job Enter Stage error: jobid %s stage %s error %s" %
+                                              (job.jobid, scheduler_stage, e))
 
-                toremove = []
-                cycle_commands = set()
-                for job in self.jobqueue:
-                    if not job.disabled and (job.stage is None or job.stage == scheduler_stage):
+                '''
+                Main scheduler duty. Every job is executed from the priority queue. Every job can add elements to
+                cycle_commands set. Atm the scheduler handle 'nextstage' and 'shutdown' command, but you can return
+                a custom command that will be handled ad end of the scheduler cycle.
+                '''
+                for job in cloned_jobs:
+                    try:
                         job.execute(self.telemetry, self, scheduler_stage, cycle_commands)
-                    if job.expired:
-                        toremove.append(job)
+                    except Exception as e:
+                        logging.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
 
-                # remove expired elements
-                for job in toremove:
-                    self.jobqueue.remove(job)
-
-                # Execute scheduler cycle command
-                for cmd in cycle_commands:
-                    if cmd == 'nextstage':
-                        if scheduler_stage == self.telemetry.stage():
-                            self.vessel.control.activate_next_stage()
-                    elif cmd == 'shutdown':
-                        self.shutdown = True
-                    else:
-                        # custom command!
-                        for job in self.jobqueue:
-                            if not job.disabled and (job.stage is None or job.stage == scheduler_stage):
+            '''
+            Execute cycle commands added from the jobs. Nextstage and shutdown are handled by the scheduler and
+            will not be propagated to jobs.
+            '''
+            for cmd in cycle_commands:
+                logging.debug('Command %s triggered' % cmd)
+                if cmd == 'nextstage':
+                    if scheduler_stage == self.telemetry.stage():
+                        self.vessel.control.activate_next_stage()
+                elif cmd == 'shutdown':
+                    self.shutdown = True
+                else:
+                    for job in cloned_jobs:
+                        if not job.disabled and not job.expired:
+                            try:
                                 job.execute_custom_command(self.telemetry, self, scheduler_stage, cmd)
+                            except Exception as e:
+                                logging.error("Job Execution Error: jobid %s error %s" % (job.jobid, e))
 
-                time.sleep(self.frequency)
-                self.queuelock.release()
-            except Exception as e:
-                self.queuelock.release()
-                print(e)
-                time.sleep(1)
+            time.sleep(self.frequency)
 
 
 
